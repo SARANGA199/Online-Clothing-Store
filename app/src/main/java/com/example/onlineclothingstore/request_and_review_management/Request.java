d14d8b80ec1e00835ee0a454658d04972b795d07
Tharu-Dissanayaka <dtharushi54@gmail.com>
2021-09-19 13:00:38 +0530

    v1

--- app/src/main/java/com/example/onlineclothingstore/request_and_review_management/Request.java
+++ app/src/main/java/com/example/onlineclothingstore/request_and_review_management/Request.java
@@ -12,5 +12,5 @@ public class Request extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request);
-    }
+    }//vfddds
 }
