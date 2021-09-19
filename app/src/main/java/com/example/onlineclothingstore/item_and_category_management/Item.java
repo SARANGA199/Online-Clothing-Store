496dfe144e12de562ffde97b1995b62e380ac196
Pathiranage Vishwa <it20260910@my.sliit.lk>
2021-09-19 11:38:39 +0530

    initial

---
+++ app/src/main/java/com/example/onlineclothingstore/item_and_category_management/Item.java
@@ -0,0 +1,16 @@
+package com.example.onlineclothingstore.item_and_category_management;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.onlineclothingstore.R;
+
+public class Item extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item);
+    }
+}
---
+++ app/src/main/java/com/example/onlineclothingstore/user_and_payment_management/User.java
@@ -0,0 +1,16 @@
+package com.example.onlineclothingstore.user_and_payment_management;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.onlineclothingstore.R;
+
+public class User extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+    }
+}
