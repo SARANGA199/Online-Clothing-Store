8919de297b253df0ab2083885a586320d716a7c5
SARANGA199 <IT20260460@my.sliit.lk>
2021-09-24 02:22:39 +0530

    V11

--- app/src/main/java/com/example/onlineclothingstore/item_and_category_management/AddItem.java
+++ app/src/main/java/com/example/onlineclothingstore/item_and_category_management/AddItem.java
@@ -1,16 +1,117 @@
 package com.example.onlineclothingstore.item_and_category_management;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.example.onlineclothingstore.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class AddItem extends AppCompatActivity {
+
+    FirebaseDatabase mDatabase;
+    DatabaseReference mRef;
+    FirebaseStorage mStorage;
+    ImageButton imageButton;
+    EditText iname,iprice,ibrand;
+    Button button;
+
+    private static final int Gallery_Code=1;
+    Uri imageUrl= null;
+    ProgressDialog progressDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+
+        imageButton = findViewById(R.id.addIbtn);
+        iname= findViewById(R.id.iname);
+        iprice= findViewById(R.id.iprice);
+        ibrand= findViewById(R.id.ibrand);
+        button= findViewById(R.id.btnIadd);
+
+        mDatabase = FirebaseDatabase.getInstance();
+        mRef = mDatabase.getReference().child("Items");
+        mStorage = FirebaseStorage.getInstance();
+        progressDialog = new ProgressDialog(this);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent,Gallery_Code);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == Gallery_Code && resultCode==RESULT_OK){
+            imageUrl = data.getData();
+            imageButton.setImageURI(imageUrl);
+        }
+
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name= iname.getText().toString().trim();
+                String price = iprice.getText().toString().trim();
+                String brand = ibrand.getText().toString().trim();
+
+                if(!(name.isEmpty() &&price.isEmpty() && imageUrl!=null)){
+
+                    progressDialog.setTitle("Uploading...");
+                    progressDialog.show();
+
+                    StorageReference filepath = mStorage.getReference().child("imagePost").child(imageUrl.getLastPathSegment());
+                    filepath.putFile(imageUrl).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Task<Uri> downloadUrl = taskSnapshot.getStorage().getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Uri> task) {
+                                    String t = task.getResult().toString();
+
+                                    DatabaseReference newPost = mRef.push();
+
+                                    newPost.child("ItemName").setValue(name);
+                                    newPost.child("ItemPrice").setValue(price);
+                                    newPost.child("ItemBrand").setValue(brand);
+                                    newPost.child("image").setValue(t);
+                                    progressDialog.dismiss();
+
+                                    Toast.makeText(AddItem.this, "Item Added Successfully", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                    });
+                }
+            }
+        });
     }
 }
