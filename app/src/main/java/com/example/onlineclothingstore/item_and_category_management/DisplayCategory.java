af3487c5d4a3295fb4d3392cb24850696f0e2e3f
SARANGA199 <IT20260460@my.sliit.lk>
2021-09-23 10:55:34 +0530

    V9

--- app/src/main/java/com/example/onlineclothingstore/item_and_category_management/DisplayCategory.java
+++ app/src/main/java/com/example/onlineclothingstore/item_and_category_management/DisplayCategory.java
@@ -1,6 +1,11 @@
 package com.example.onlineclothingstore.item_and_category_management;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -14,27 +19,54 @@ import android.widget.SearchView;
 
 import com.example.onlineclothingstore.R;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class DisplayCategory extends AppCompatActivity {
+public class DisplayCategory extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     RecyclerView recyclerView;
     CategoryAdapter categoryAdapter;
     Button btnAdd;
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_category);
 
-        btnAdd = findViewById(R.id.btnAdd);
+        //Main Menu
+        toolbar = findViewById(R.id.toolbar);
 
-        btnAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(DisplayCategory.this,AddCategory.class));
-            }
-        });
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+
+        setSupportActionBar(toolbar);
+
+        //Navigation Drawer Menu
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_home);
+
+
+
+
+
+
+//        btnAdd = findViewById(R.id.btnAdd);
+//
+//        btnAdd.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(DisplayCategory.this,AddCategory.class));
+//            }
+//        });
 
         recyclerView = (RecyclerView) findViewById(R.id.rv);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -62,43 +94,74 @@ public class DisplayCategory extends AppCompatActivity {
     }
 
 
-    //search
+
+    //to  stop app get close when pressing back key
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public void onBackPressed(){
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+    }
 
-        getMenuInflater().inflate(R.menu.search,menu);
+     //Menu Item select
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-        MenuItem item= menu.findItem(R.id.search);
-        SearchView searchView =(SearchView)item.getActionView();
 
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                txtSearch(query);
-                return false;
-            }
+        switch (item.getItemId()){
+            case R.id.nav_home:
+                break;
+            case R.id.nav_category:
+                Intent int1 =new Intent(DisplayCategory.this,AddCategory.class);
+                startActivity(int1);
+                break;
+        }
 
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                txtSearch(newText);
-                return false;
-            }
-        });
-
-        return super.onCreateOptionsMenu(menu);
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 
 
-    private void txtSearch(String str){
-
-
-        FirebaseRecyclerOptions<CategoryModule>  options=
-                new FirebaseRecyclerOptions.Builder<CategoryModule>()
-                        .setQuery(FirebaseDatabase.getInstance().getReference().child("Categories").orderByChild("category").startAt(str).endAt(str+"~"),CategoryModule.class)
-                        .build();
-
-        categoryAdapter = new CategoryAdapter(options);
-        categoryAdapter.startListening();
-        recyclerView.setAdapter(categoryAdapter);
-    }
+//
+//    //search
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//
+//        getMenuInflater().inflate(R.menu.search,menu);
+//
+//        MenuItem item= menu.findItem(R.id.search);
+//        SearchView searchView =(SearchView)item.getActionView();
+//
+//        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+//            @Override
+//            public boolean onQueryTextSubmit(String query) {
+//                txtSearch(query);
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean onQueryTextChange(String newText) {
+//                txtSearch(newText);
+//                return false;
+//            }
+//        });
+//
+//        return super.onCreateOptionsMenu(menu);
+//    }
+//
+//
+//    private void txtSearch(String str){
+//
+//
+//        FirebaseRecyclerOptions<CategoryModule>  options=
+//                new FirebaseRecyclerOptions.Builder<CategoryModule>()
+//                        .setQuery(FirebaseDatabase.getInstance().getReference().child("Categories").orderByChild("category").startAt(str).endAt(str+"~"),CategoryModule.class)
+//                        .build();
+//
+//        categoryAdapter = new CategoryAdapter(options);
+//        categoryAdapter.startListening();
+//        recyclerView.setAdapter(categoryAdapter);
+//    }
 }
